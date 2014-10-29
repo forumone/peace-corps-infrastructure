292fe1cde8c09dd00350cc6b61797541f7f9da85
Sean Herron <sean.herron@gsa.gov>
2014-10-29 17:30:44 -0400

    added better enc key handling

--- ansible/playbooks/update_ansible.sh
+++ ansible/playbooks/update_ansible.sh
@@ -5,7 +5,9 @@ aws s3 cp s3://peacecorps-secrets secrets/ --recursive
 echo -n "Enter the secret key for decryption and press [ENTER]: "
 read key
 
+export PC_ENC_KEY=$key
+
 for filename in secrets/*
 do
-    openssl aes-256-cbc -d -a -salt -pass pass:$key -in $filename -out $filename.enc
+    openssl aes-256-cbc -d -a -salt -pass env:PC_ENC_KEY -in $filename -out $filename.enc
 done;
