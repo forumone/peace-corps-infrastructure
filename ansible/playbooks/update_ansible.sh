321f2ff7940b794ab997254f4ed0da834dd2cfce
Sean Herron <sean.herron@gsa.gov>
2014-10-29 17:19:51 -0400

    update ansible script

---
+++ ansible/playbooks/update_ansible.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git pull origin master
+aws s3 cp s3://peacecorps-secrets secrets/ --recursive
+
+echo -n "Enter the secret key for decryption and press [ENTER]: "
+read key
+
+for filename in secrets/*
+do
+    openssl aes-256-cbc -d -a -salt -pass $key -in $filename -out $filename.enc
+done;
