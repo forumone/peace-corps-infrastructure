d79ec52165a8f782cc8819f1c57a135d487803ed
Sean Herron <sean.herron@gsa.gov>
2014-10-29 17:40:07 -0400

    update and move script

--- ansible/playbooks/update_ansible.sh
+++ ansible/playbooks/update_ansible.sh
@@ -11,3 +11,9 @@ for filename in secrets/*.enc
 do
     openssl aes-256-cbc -d -a -salt -pass env:PC_ENC_KEY -in $filename -out $filename.unencrypted
 done;
+
+# We have to go file by file for this, which kind of sucks!
+cp secrets/deploy.enc.unencrypted deploy.pem
+chmod 600 deploy.pem
+
+cp secrets/all.secret.unencrypted group_vars/all
--- deploy_key_init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-openssl aes-256-cbc -d -a -salt -in ~/deploy.enc -out ~/deploy
-chmod 600 ~/deploy
