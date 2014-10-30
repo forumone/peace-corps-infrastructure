9522f9b27ca5606cdd12aecb3ee5764e0b8e80af
Sean Herron <sean.herron@gsa.gov>
2014-10-30 17:25:10 -0400

    nginx SSL

--- ansible/update_ansible.sh
+++ ansible/update_ansible.sh
@@ -20,6 +20,9 @@ chmod 600 deploy.pem
 
 cp secrets/all.secret.enc.unencrypted playbooks/group_vars/all
 
+cp secrets/pay.donate.crt.enc.unencrypted playbooks/roles/nat/files/pay.donate.crt
+cp secrets/pay.donate.peacecorps-dev.18f.us.key.enc.unencrypted playbooks/roles/nat/files/pay.donate.peacecorps-dev.18f.us.key
+
 cp secrets/peacecorps.pem.pub playbooks/roles/nat/files/peacecorps.pem.pub
 cp secrets/cm.pem.pub playbooks/roles/nat/files/cm.pem.pub
 
