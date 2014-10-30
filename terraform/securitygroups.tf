54e9d466aecf8d4914323727e8bbd49b30ea95c4
Sean Herron <sean.herron@gsa.gov>
2014-10-30 16:53:42 -0400

    lineinfile

--- terraform/securitygroups.tf
+++ terraform/securitygroups.tf
@@ -63,6 +63,13 @@ resource "aws_security_group" "admin" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.19.61.0/0"]
+  }
 }
 
 resource "aws_security_group" "rds" {
