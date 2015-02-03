5e3382014d5b5efedb325250735fa4995d770896
CM Lubinski <christopher.lubinski@gsa.gov>
2015-02-03 02:37:03 +0000

    Tweak text a bit

--- ansible/playbooks/roles/filetransfer/files/check_arrived.sh
+++ ansible/playbooks/roles/filetransfer/files/check_arrived.sh
@@ -4,7 +4,7 @@ source /home/filetransfer/no_file_emails.sh
 
 if [[ -z $(find /home/filetransfer/incoming/*.csv -mtime 0) ]]; then
   message="Subject: No file in 24 hours\n\n"
-  message="$message The latest files (files deleted every 7 days):\n"
-  message="$message `ls /home/filetransfer/incoming/*.csv`"
-  echo -e $message | sendmail -f "Donate Server <noreply@donate.peacecorps.gov>" $NO_FILE_EMAILS
+  message="$message The latest files (files deleted every 7 days):\n\n"
+  message="$message `ls -l /home/filetransfer/incoming/*.csv`"
+  echo -e $message | sendmail -F "Donate Server" -f noreply@donate.peacecorps.gov $NO_FILE_EMAILS
 fi
