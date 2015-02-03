a2b9068334efd768e3b3c6d60830b5333f34d384
CM Lubinski <christopher.lubinski@gsa.gov>
2015-02-03 05:00:46 +0000

    Many escapes

--- ansible/playbooks/roles/filetransfer/files/check_arrived.sh
+++ ansible/playbooks/roles/filetransfer/files/check_arrived.sh
@@ -5,6 +5,6 @@ source /home/filetransfer/no_file_emails.sh
 if [[ -z $(find /home/filetransfer/incoming/*.csv -mtime 0) ]]; then
   message="To: $NO_FILE_EMAILS\nSubject: No file in 24 hours\n\n"
   message="$message The latest files (files deleted every 7 days):\n\n"
-  message="$message `ls -l /home/filetransfer/incoming/*.csv | awk '{print $9" UTC:"$6" "$7" "$8"\n"}'`"
+  message="$message `ls -l /home/filetransfer/incoming/*.csv | awk '{print $9" UTC:"$6" "$7" "$8"\\n"}'`"
   echo -e $message | sendmail -F "Donate Server" -f noreply@donate.peacecorps.gov -t
 fi
