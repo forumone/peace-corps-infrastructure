5f9ea7dacbc8af51e24e1e8fb79a130bd0f4bb06
CM Lubinski <christopher.lubinski@gsa.gov>
2015-02-03 16:31:44 +0000

    Loop over lines

--- ansible/playbooks/roles/filetransfer/files/check_arrived.sh
+++ ansible/playbooks/roles/filetransfer/files/check_arrived.sh
@@ -5,6 +5,10 @@ source /home/filetransfer/no_file_emails.sh
 if [[ -z $(find /home/filetransfer/incoming/*.csv -mtime 0) ]]; then
   message="To: $NO_FILE_EMAILS\nSubject: No file in 24 hours\n\n"
   message="$message The latest files (files deleted every 7 days):\n\n"
-  message="$message `ls -l /home/filetransfer/incoming/*.csv | awk '{print $9" UTC:"$6" "$7" "$8"\\n"}'`"
+  ls -l /home/filetransfer/incoming/*.csv | awk '{print $9" UTC:"$6" "$7" "$8}' | while read line
+  do
+    message="$message$line\n"
+  done
+
   echo -e $message | sendmail -F "Donate Server" -f noreply@donate.peacecorps.gov -t
 fi
