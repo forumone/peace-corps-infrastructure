e625b9fdd1d20b5d0387b0e7f858447cdecdf403
Sean Herron <sean@herron.io>
2015-02-22 16:29:07 -0500

    trying regex for ^/$

--- packer/files/admin/init.sh
+++ packer/files/admin/init.sh
@@ -4,7 +4,7 @@ sed -i "s@spdy proxy_protocol@spdy@" /etc/nginx/sites-enabled/peacecorps
 sed -i "s@set_real_ip_from@# set_real_ip_from@" /etc/nginx/sites-enabled/peacecorps
 sed -i "s@real_ip_header@#real_ip_header@" /etc/nginx/sites-enabled/peacecorps
 sed -i "s@ elb_log;@; # elb_log;@" /etc/nginx/sites-enabled/peacecorps
-sed -i "s@deny all; # Deny all unless you are on an admin server@# Admin Server Enabled@" /etc/nginx/sites-enabled/peacecorps
+sed -i '/location \/admin {/,/}/d' /etc/nginx/sites-enabled/peacecorps
 service nginx restart
 
 cp /home/ubuntu/files/admin/admin_vars.sh /home/peacecorps/admin_vars.sh
